bdc34b367a3beaab0a0f32216669a319e6c704a6
Minh Dinh <minh.dinhcong@gameloft.com>
2024-02-22 18:05:06 +0700

    Finish create SwapChain Finish create Image View

--- TriangleApplication.cpp
+++ TriangleApplication.cpp
@@ -25,6 +25,10 @@ void TriangleApplication::initVulkan()
 	createSurface(); //Should be call right after create vulkan instance;
 	selectPhysicalDevice(); //Select a suitable physical device
 	createLogicalDevice(); //Create a logical device to interface with the physical device
+	createSwapChain(); //Create swapchain after checking for GPU extension and surface capabilities
+	getSwapChainImages();
+	createImageView(); //To use the image in the swapchain, we have to create the image view
+	createGraphicsPipeline(); //To create a graphic pipeline to bring image to screen. Input --> pipeline --> screen
 }
 
 void TriangleApplication::mainLoop()
@@ -38,6 +42,11 @@ void TriangleApplication::mainLoop()
 
 void TriangleApplication::cleanup()
 {
+	for (const auto& imageView : m_imageViews)
+	{
+		vkDestroyImageView(m_device, imageView, nullptr);
+	}
+	vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
 	vkDestroyDevice(m_device, nullptr);
 	vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
 	vkDestroyInstance(m_instance, nullptr);//Instance should be destroyed last
@@ -89,37 +98,13 @@ void TriangleApplication::createVkInstance()
 	}
 }
 
-//Function to check if ALL of the requested layers are available
-bool TriangleApplication::checkValidationLayerSupport()
+void TriangleApplication::createSurface()
 {
-	// Get all available layers
-	uint32_t layerCount = 0;
-	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);//get the size
-
-	std::vector<VkLayerProperties> availableLayers(layerCount);//pass the size to the vector
-	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
-
-	//Check if all requested layers are contained in the available layers
-	for (const char* layerName : validationLayers)
+	if (glfwCreateWindowSurface(m_instance, m_window, nullptr, &m_surface) != VK_SUCCESS)
 	{
-		bool layerFound = false;
-
-		for (const auto& layerProperties : availableLayers)
-		{
-			if (strcmp(layerName, layerProperties.layerName) == 0)
-			{
-				layerFound = true;
-				break;
-			}
-		}
-
-		if (!layerFound)
-		{
-			return false; //Return false if exist 1 layer not found in the available layers
-		}
+		throw std::runtime_error("failed to create window surface");
 	}
 
-	return true;
 }
 
 void TriangleApplication::selectPhysicalDevice()
@@ -155,36 +140,16 @@ void TriangleApplication::selectPhysicalDevice()
 	}
 }
 
-//Function to evaluate the suitability of the device
-bool TriangleApplication::isDeviceSuitable(VkPhysicalDevice device){
-	return true; //for first simple application
-
-	//Get properties and feature of the physical device
-	VkPhysicalDeviceProperties deviceProperties;
-	vkGetPhysicalDeviceProperties(device, &deviceProperties);
-
-	VkPhysicalDeviceFeatures deviceFeatures;
-	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-
-	//Check if the physical device is suitable for our own application requirement
-	
-	//Find the queue family
-	QueueFamilyIndices indices = findQueueFamilies(device);
-
-	//Find the suitable device
-	return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader && indices.isComplete(); //App needs device has discrete GPU and available for geometryShader
-}
-
 void TriangleApplication::createLogicalDevice()
 {
 	//Prepare queue info
-	QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
+	vkAppStruct::QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
 
 	// As we are having 2 queues: 1 for input and 1 for output
 	std::vector<VkDeviceQueueCreateInfo> queueInfos;
-	std::set<uint32_t> uniqueQueueFamilies{ 
-		indices.graphicsFamily.value(), 
-		indices.presentFamily.value() 
+	std::set<uint32_t> uniqueQueueFamilies{
+		indices.graphicsFamily.value(),
+		indices.presentFamily.value()
 	};
 
 	float queuePriority = 1.0f;
@@ -197,10 +162,10 @@ void TriangleApplication::createLogicalDevice()
 		queueInfo.queueCount = 1;//Uses 1 queue only
 		queueInfo.pNext = nullptr;
 		queueInfo.pQueuePriorities = &queuePriority;
-	
+
 		queueInfos.push_back(queueInfo);
 	}
-	
+
 
 	//Prepare device features
 	VkPhysicalDeviceFeatures deviceFeatures{};
@@ -214,11 +179,15 @@ void TriangleApplication::createLogicalDevice()
 
 	deviceInfo.pEnabledFeatures = &deviceFeatures;
 
+	//set up validation layer
 	deviceInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
 	deviceInfo.ppEnabledLayerNames = validationLayers.data();
 	deviceInfo.pNext = nullptr;
 
-	deviceInfo.enabledExtensionCount = 0;
+	//set up extension
+	deviceInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+	deviceInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
 	VkResult result = vkCreateDevice(m_physicalDevice, &deviceInfo, nullptr, &m_device);
 	if (result != VK_SUCCESS)
 	{
@@ -234,9 +203,192 @@ void TriangleApplication::createLogicalDevice()
 	vkGetDeviceQueue(m_device, indices.presentFamily.value(), 0, &m_presentQueue);
 }
 
-TriangleApplication::QueueFamilyIndices TriangleApplication::findQueueFamilies(VkPhysicalDevice device)
+void TriangleApplication::createSwapChain()
 {
-	QueueFamilyIndices indices;
+	//Get swapchain support detail
+	m_swapchainDetails = findSwapChainSupportDetails(m_physicalDevice);
+
+	//Select the properties from available ones
+	m_format = selectSurfaceDisplayFormat(m_swapchainDetails.formats);
+	m_presentMode = selectSurfaceDisplayMode(m_swapchainDetails.presentModes);
+	VkExtent2D extent = selectSurfaceDisplayExtent(m_swapchainDetails.capabilities);
+
+	uint32_t imageCount = m_swapchainDetails.capabilities.minImageCount + 1; //Avoid GPU internal waiting
+	if (m_swapchainDetails.capabilities.maxImageCount > 0 && imageCount > m_swapchainDetails.capabilities.maxImageCount)
+	{
+		imageCount = m_swapchainDetails.capabilities.maxImageCount; // min(maxImageCount, minImageCount + 1)
+	}
+
+	VkSwapchainCreateInfoKHR createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	createInfo.surface = m_surface;
+
+	createInfo.minImageCount = imageCount;
+	createInfo.imageFormat = m_format.format;
+	createInfo.imageColorSpace = m_format.colorSpace;
+
+	createInfo.imageExtent = extent;
+
+	createInfo.imageArrayLayers = 1;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	//[Device] in-->[queue families]-->out [Surface]
+	//Select how to handle the image in the swap chain
+
+	m_queueIndices = findQueueFamilies(m_physicalDevice);
+	uint32_t QueueFamilyIndices[] = { m_queueIndices.graphicsFamily.value(), m_queueIndices.presentFamily.value() };
+
+	if (m_queueIndices.graphicsFamily.value() == m_queueIndices.presentFamily.value()) //using one families for both in and out --> image should be exclusively belonging to that
+	{
+		createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		createInfo.queueFamilyIndexCount = 2;
+		createInfo.pQueueFamilyIndices = QueueFamilyIndices;
+	}
+	else //different
+	{
+		createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+		createInfo.queueFamilyIndexCount = 0; //optional
+		createInfo.pQueueFamilyIndices = nullptr; //optional
+	}
+
+	createInfo.preTransform = m_swapchainDetails.capabilities.currentTransform; //Rotating for example
+
+	createInfo.presentMode = m_presentMode;
+	createInfo.clipped = VK_TRUE; //dont care about the color pixels that are obscured. E.g: another window is in front of them
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+
+	createInfo.oldSwapchain = VK_NULL_HANDLE; //handle the old swapchain in case it is recreated
+
+	if (vkCreateSwapchainKHR(m_device, &createInfo, nullptr, &m_swapchain) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create swapchain");
+	}
+}
+
+void TriangleApplication::createImageView() {
+	
+	m_imageViews.resize(m_swapchainImages.size()); // Reserve maximum capacity = all images in the swapchain
+	for (int i = 0; i < m_swapchainImages.size(); i++)
+	{
+		VkImageViewCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image = m_swapchainImages[i];
+
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = m_format.format;
+
+		createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+		createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		createInfo.subresourceRange.baseMipLevel = 0;
+		createInfo.subresourceRange.levelCount = 1;
+		createInfo.subresourceRange.baseArrayLayer = 0;
+		createInfo.subresourceRange.layerCount = 1; //3D apps
+
+		if (vkCreateImageView(m_device, &createInfo, nullptr, &m_imageViews[i]) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create image view");
+		}
+	}
+}
+
+void TriangleApplication::createGraphicsPipeline()
+{
+	//Read the shaders: vertext shader and fragment shader
+}
+//Function to evaluate the suitability of the device
+bool TriangleApplication::isDeviceSuitable(VkPhysicalDevice device){
+	//return true; //for first simple application
+
+	//Get properties and feature of the physical device
+	VkPhysicalDeviceProperties deviceProperties;
+	vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+	VkPhysicalDeviceFeatures deviceFeatures;
+	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+	//Check if the physical device is suitable for our own application requirement
+	
+	//Find the queue family
+	vkAppStruct::QueueFamilyIndices indices = findQueueFamilies(device);
+
+	//Check if Swapchain is supported by both GPU and Surface
+	bool isExtensionSupported = checkDeviceExtensionSupport(device);
+	bool isSwapChainSurfaceSupported = false;
+	if (isExtensionSupported)
+	{
+		vkAppStruct::SwapChainSupportDetails details = findSwapChainSupportDetails(device);
+		isSwapChainSurfaceSupported = !details.formats.empty() && !details.presentModes.empty();
+	}
+
+	//Find the suitable device
+	return 
+		deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && 
+		deviceFeatures.geometryShader && 
+		indices.isComplete() && 
+		isExtensionSupported &&
+		isSwapChainSurfaceSupported; //App needs device has discrete GPU and available for geometryShader
+}
+//Function to check if ALL of the requested layers are available
+bool TriangleApplication::checkValidationLayerSupport()
+{
+	// Get all available layers
+	uint32_t layerCount = 0;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);//get the size
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);//pass the size to the vector
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	//Check if all requested layers are contained in the available layers
+	//for (const char* layerName : validationLayers)
+	//{
+	//	bool layerFound = false;
+
+	//	for (const auto& layerProperties : availableLayers)
+	//	{
+	//		if (strcmp(layerName, layerProperties.layerName) == 0)
+	//		{
+	//			layerFound = true;
+	//			break;
+	//		}
+	//	}
+
+	//	if (!layerFound)
+	//	{
+	//		return false; //Return false if exist 1 layer not found in the available layers
+	//	}
+	//}
+	std::set<std::string> requiredLayers(validationLayers.begin(), validationLayers.end());
+	for (const auto& layer : availableLayers)
+	{
+		requiredLayers.erase(layer.layerName);
+	}
+	return requiredLayers.empty();
+}
+
+bool TriangleApplication::checkDeviceExtensionSupport(VkPhysicalDevice device)
+{
+	//Get the number of extension supported by the device
+	uint32_t extensionCount = 0;
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+	std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+	std::set<std::string> requiredExtension(deviceExtensions.begin(), deviceExtensions.end()); //copy the name of the extension from the device extension vector
+	for (const auto& extension : availableExtensions)
+	{
+		requiredExtension.erase(extension.extensionName); //if the required one is contained in the available -> remove it
+	}
+	return requiredExtension.empty(); //if all required extensions are available, they will be removed
+}
+
+vkAppStruct::QueueFamilyIndices TriangleApplication::findQueueFamilies(VkPhysicalDevice device)
+{
+	vkAppStruct::QueueFamilyIndices indices;
 	//logic to find queue family indices
 
 	//Get the queue families of a device
@@ -258,7 +410,7 @@ TriangleApplication::QueueFamilyIndices TriangleApplication::findQueueFamilies(V
 
 		// Check for output queue available
 		VkBool32 isPresentSupported = false;
-		vkGetPhysicalDeviceSurfaceSupportKHR(m_physicalDevice, index, m_surface, &isPresentSupported);
+		vkGetPhysicalDeviceSurfaceSupportKHR(device, index, m_surface, &isPresentSupported);
 		if (isPresentSupported)
 		{
 			indices.presentFamily = index;
@@ -273,11 +425,104 @@ TriangleApplication::QueueFamilyIndices TriangleApplication::findQueueFamilies(V
 	return indices;
 }
 
-void TriangleApplication::createSurface()
+vkAppStruct::SwapChainSupportDetails TriangleApplication::findSwapChainSupportDetails(VkPhysicalDevice device)
 {
-	if (glfwCreateWindowSurface(m_instance, m_window, nullptr, &m_surface) != VK_SUCCESS)
+	//Required data before a swapchain could be created.
+	vkAppStruct::SwapChainSupportDetails details;
+	//logic to find the detail
+
+	//Get the surface capabilities
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, m_surface, &details.capabilities); 
+
+	uint32_t formatCounts = 0;
+	uint32_t presentModeCounts = 0;
+
+	vkGetPhysicalDeviceSurfaceFormatsKHR(device, m_surface, &formatCounts, nullptr);
+	vkGetPhysicalDeviceSurfacePresentModesKHR(device, m_surface, &presentModeCounts, nullptr);
+
+	if (formatCounts != 0)
 	{
-		throw std::runtime_error("failed to create window surface");
+		details.formats.resize(formatCounts);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, m_surface, &formatCounts, details.formats.data());
 	}
 
+	if (presentModeCounts != 0)
+	{
+		details.presentModes.resize(presentModeCounts);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, m_surface, &presentModeCounts, details.presentModes.data());
+	}
+
+	return details;
+}
+
+VkSurfaceFormatKHR TriangleApplication::selectSurfaceDisplayFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+{
+	// Each VkSurfaceFormatKHR struct contains: format and color space
+	for (const auto& format : availableFormats)
+	{
+		if (format.format == VK_FORMAT_R8G8B8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+		{
+			return format;
+		}
+	}
+
+	return availableFormats[0];
+}
+
+VkPresentModeKHR TriangleApplication::selectSurfaceDisplayMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
+{
+	for (const auto& mode : availablePresentModes)
+	{
+		if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
+		{
+			return mode;
+		}
+	}
+	return VK_PRESENT_MODE_FIFO_KHR; //Default
+}
+
+VkExtent2D TriangleApplication::selectSurfaceDisplayExtent(const VkSurfaceCapabilitiesKHR& capabilities)
+{
+	//Select the resolution between the available range
+	//Usually equal to the resolution of the window - in pixels
+	//GLFW has 2 measurements for resolution: pixels and screen coordinate
+	//Vulkan works with pixels - use glfwGetFrameBufferSize to get the pixels of the window/buffer
+	if (capabilities.currentExtent.width != /*std::numeric_limits<uint32_t>::max()*/UINT32_MAX)
+	{
+		return capabilities.currentExtent; //the current resolution of the window is indicated in currentExtent
+	}
+	else //if the window managers do allow us to modify the extent to best match with the min/max possible extent -> the wd managers will set the currentExtent to max
+	{
+		int width, height;
+		glfwGetFramebufferSize(m_window, &width, &height); //Get the window resolution in pixels
+
+		VkExtent2D actualExtent
+		{
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+		};
+
+		//clamp the actual width and height of the window to be bounded by min/maxImageExtent
+		actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+}
+
+void TriangleApplication::getSwapChainImages()
+{
+	uint32_t imageCount = 0;
+	vkGetSwapchainImagesKHR(m_device, m_swapchain, &imageCount, nullptr);
+
+	if (imageCount > 0)
+	{
+		m_swapchainImages.resize(imageCount);
+		vkGetSwapchainImagesKHR(m_device, m_swapchain, &imageCount, m_swapchainImages.data());
+		std::cout << "Swapchain has " << imageCount << " images" << std::endl;
+	}
+	else
+	{
+		throw std::runtime_error("Empty swapchain");
+	}
 }
--- TriangleApplication.h
+++ TriangleApplication.h
@@ -12,29 +12,15 @@
 #include <optional>
 #include <set>
 
+#include <cstdint>
+#include <limits>
+#include <algorithm> //std::clamp
+
 #define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
 
-class TriangleApplication
-{
-	//define the methods
-public:
-	void run();
-
-private:
-	GLFWwindow* m_window;
-	VkInstance m_instance;
-	VkPhysicalDevice m_physicalDevice;
-	VkDevice m_device; //logical device handler
-
-	VkQueue m_graphicQueue; //handler of the graphic queue
-	VkQueue m_presentQueue; //handler of the present queue
-
-	VkSurfaceKHR m_surface;
-
-	const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
-
+namespace vkAppStruct {
 	struct QueueFamilyIndices
 	{
 		std::optional<uint32_t> graphicsFamily; //Queue family of the device that allows graphic commands - Input
@@ -47,16 +33,73 @@ private:
 		}
 	};
 
+	struct SwapChainSupportDetails
+	{
+		VkSurfaceCapabilitiesKHR capabilities; // Capabilities of the surface: min/max number of images in a swapchain - min/max size of an image
+		std::vector<VkSurfaceFormatKHR> formats; // Format of the image that the surface can display
+		std::vector<VkPresentModeKHR> presentModes; // Present modes available for the surface
+	};
+}//end vkAppStruct
+class TriangleApplication
+{
+	//define the methods
+public:
+	void run();
+
+private:
+	GLFWwindow* m_window;
+	VkInstance m_instance;
+	VkPhysicalDevice m_physicalDevice;
+	VkDevice m_device; //logical device handler
+	VkSwapchainKHR m_swapchain;
+
+	VkQueue m_graphicQueue; //handler of the graphic queue
+	VkQueue m_presentQueue; //handler of the present queue
+
+	VkSurfaceKHR m_surface;
+	
+	vkAppStruct::QueueFamilyIndices m_queueIndices;
+	vkAppStruct::SwapChainSupportDetails m_swapchainDetails;
+
+	VkSurfaceFormatKHR m_format;
+	VkPresentModeKHR m_presentMode;
+
+	VkPipeline m_pipeline;
+
+	const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+	const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	std::vector<VkImage> m_swapchainImages;
+	std::vector<VkImageView> m_imageViews; //Each image requires an image view to be presented. Describes: 1. How to access image 2. Which part to access
+
 	void initWindow();
 	void initVulkan();
 	void mainLoop();
 	void cleanup();
 
 	void createVkInstance();
-	bool checkValidationLayerSupport();
-	void selectPhysicalDevice();
-	bool isDeviceSuitable(VkPhysicalDevice device);
-	void createLogicalDevice();
-	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 	void createSurface();
+	void selectPhysicalDevice();
+	void createLogicalDevice();
+	void createSwapChain();
+	void createImageView();
+	void createGraphicsPipeline(); //a pipelline is created to draw an image to screen
+
+	// helper function to check the suitability of the physical devices
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device); //GPU should support swapchain : Device ---> |  swap chain | ---> screen / surface
+	
+	
+	// helper function to query the required features
+	vkAppStruct::QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+	vkAppStruct::SwapChainSupportDetails findSwapChainSupportDetails(VkPhysicalDevice device);
+
+	// helper functions to select the appropriated display format and mode
+	VkSurfaceFormatKHR selectSurfaceDisplayFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR selectSurfaceDisplayMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D selectSurfaceDisplayExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
+	// helper functions to retrieve the images in the swapchain
+	void getSwapChainImages();
 };
